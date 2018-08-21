18df7d2f3a94dc819cc232b5a6e264e18e12a9e9
Markus Tzoe <chou.marcus@gmail.com>
2018-08-21 12:12:06 +0800

    cmd list

--- main.go
+++ main.go
@@ -25,16 +25,29 @@ func init() {
 
 func main() {
 	flag.Parse()
+	if flag.NArg() == 0 {
+		fmt.Fprintf(os.Stderr, "usage: argo {CMD} {PARAMETERS}...\n")
+		flag.PrintDefaults()
+		fmt.Fprintln(os.Stderr, "available cmds:")
+		k := 0
+		cmdlist := make([][3]string, len(cmds)/3+1)
+		for cmd := range cmds {
+			i := k / 3
+			j := k % 3
+			cmdlist[i][j] = cmd
+			k++
+		}
+		for i := range cmdlist {
+			fmt.Fprintf(os.Stderr, "\t%s\r\t\t\t\t%s\r\t\t\t\t\t\t\t\t%s\n", cmdlist[i][0], cmdlist[i][1], cmdlist[i][2])
+		}
+		fmt.Fprintln(os.Stderr)
+		os.Exit(1)
+	}
 	var err error
 	rpcc, err = rpc.New(rpcURI, rpcSecret)
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(2)
-	}
-	if flag.NArg() == 0 {
-		fmt.Fprintf(os.Stderr, "usage: argo {CMD} {PARAMETERS}...\n")
-		flag.PrintDefaults()
-		os.Exit(1)
 	}
 	args := flag.Args()
 	if cmd, ok := cmds[args[0]]; ok {
